8fca625f52ac62cd5d9612bc8de95bad3ad3b17c
Muhammad Yameen <yameenmuhammad960@gmail.com>
2024-04-20 13:15:20 +0500

    temporarily remove admin section

--- frontend/app/(admin section)/_components/logo.jsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-function Logo() {
-  return (
-    <Image width={600} height={600} alt="logo" src="/Nustrive black.png" />
-  );
-}
-
-export default Logo;
--- frontend/app/(admin section)/admin/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Dashboard() {
-  return <div>desh</div>;
-}
-
-export default Dashboard;
