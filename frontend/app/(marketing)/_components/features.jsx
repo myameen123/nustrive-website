f28e003b1bd6283604e55998da9257689ce6f7a8
Muhammad Yameen <yameenmuhammad960@gmail.com>
2024-04-25 11:31:20 +0500

    change features

--- frontend/app/(marketing)/_components/features.jsx
+++ frontend/app/(marketing)/_components/features.jsx
@@ -4,24 +4,25 @@ function Features() {
   return (
     <div className=" flex lg:hidden bg-[#111256] mx-4 text-white md:flex-row flex-col md:p-1 sm:px-20 mt-4">
       <div className="flex flex-col gap-2 p-4 items-center border-b-[1rem] md:border-r-2 md:border-b-0">
-        <h1 className=" font-bold text-lg">NET-Enhanced Mocks!</h1>
-        <p className=" text-sm">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          accumsan leo in velit malesuada, sit amet luctus eros efficitur.{" "}
+        <h1 className=" font-bold">NET-Modeled Mock Exams</h1>
+        <p className=" text-xs">
+          Mock test is designed with the exact NET interface, making it easy for
+          you to practice and test your knowledge in different subjects.
         </p>
       </div>
       <div className="flex flex-col gap-2 p-4 items-center border-b-[1rem] md:border-r-2 md:border-b-0">
-        <h1 className=" font-bold text-lg">NET-Enhanced Mocks!</h1>
+        <h1 className=" font-bold">Comprehensive Courses</h1>
         <p className=" text-xs">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          accumsan leo in velit malesuada, sit amet luctus eros efficitur.{" "}
+          Our comprehensive courses provide in-depth learning across a wide
+          range of subjects, ensuring thorough preparation for your academic
+          journey.
         </p>
       </div>
       <div className="flex flex-col gap-2 p-4 items-center">
-        <h1 className=" font-bold text-lg">NET-Enhanced Mocks!</h1>
+        <h1 className=" font-bold">NUST Agreggate Calculator</h1>
         <p className=" text-xs">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          accumsan leo in velit malesuada, sit amet luctus eros efficitur.{" "}
+          Calculate your NUST aggregate score effortlessly with our
+          user-friendly calculator tool.
         </p>
       </div>
     </div>
