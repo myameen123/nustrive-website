1fa23652e4b7be675ac17eb89082057dc1b47844
Muhammad Yameen <yameenmuhammad960@gmail.com>
2024-07-24 19:12:29 +0500

    resolve core error

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     // origin: "http://localhost:3000", // Allow requests from this origin
-    origin: process.env.FRONTEND_URL, // Allow requests from this origin
+    // origin: process.env.FRONTEND_URL, // Allow requests from this origin
+    origin: "https://nustrive-website-backend.vercel.app", // Allow requests from this origin
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
     optionsSuccessStatus: 200,
     exposedHeaders: ["Set-cookie"],
--- frontend/app/(protected)/(admin section)/admin/page.jsx
+++ frontend/app/(protected)/(admin section)/admin/page.jsx
@@ -3,10 +3,9 @@ import { userInfo } from "@/redux/user/user-info-slice";
 import React, { useEffect } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
-import Logout from "../../_components/logout";
+// import Logout from "../../_components/logout";
 // import Logout from "../_components/logout";
 function Dashboard() {
-  const dispatch = useDispatch();
   // const { message, loading, error } = useSelector((state) => state.myInfo);
 
   // useEffect(() => {
@@ -20,14 +19,11 @@ function Dashboard() {
   //   }
   // }, [dispatch, message, loading, error?.message]);
 
-  const onClickHandler = () => {
-    dispatch(userInfo());
-  };
   return (
     <div>
       <p>desh</p>
-      <button onClick={onClickHandler}>Get info</button>
-      <Logout />
+      {/* <button onClick={onClickHandler}>Get info</button>
+      <Logout /> */}
     </div>
   );
 }
