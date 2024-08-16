20ae18f39e27a41936f1a3f42e10c39316ac08ce
alimurad21 <alimurad3422@gmail.com>
2024-08-16 12:37:46 +0500

    updated all card

--- frontend/app/(marketing)/tutors/page.jsx
+++ frontend/app/(marketing)/tutors/page.jsx
@@ -1,21 +1,21 @@
-'use Client'
-import React, { useEffect, useState } from 'react'
+// 'use Client'
+// import React, { useEffect, useState } from 'react'
 import Teachers from '../_components/teachers'
 
 const TutorPage = () => {
-  const [teachers, setTeachers] = useState();
+  // const [teachers, setTeachers] = useState();
 
-  useEffect(()=>{
-    fetchTeachers();
-  },[])
+  // useEffect(()=>{
+  //   fetchTeachers();
+  // },[])
 
-  const fetchTeachers = async () =>{
-    try{
-      const response = await axios(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/getAll/${role}`)
-    }catch(err){
-      console.log('err.message',err.message)
-    }
-  }
+  // const fetchTeachers = async () =>{
+  //   try{
+  //     const response = await axios(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/getAll/${role}`)
+  //   }catch(err){
+  //     console.log('err.message',err.message)
+  //   }
+  // }
 
   return (
     <div>TutorPage
