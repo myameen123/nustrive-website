9e46ab2b42bfde5c5f33037a99adb4b739f329e3
alimurad21 <alimurad3422@gmail.com>
2024-10-07 21:23:34 +0500

    cors added

--- backend/index.js
+++ backend/index.js
@@ -27,8 +27,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow requests from this origin
-    origin: process.env.FRONTEND_URL, // Allow requests from this origin
+    // origin: "http://localhost:3000", // Allow requests from this origin
+    origin:[ process.env.FRONTEND_URL,'https://nustrive-website-backend.vercel.app/api/'], // Allow requests from this origin
     // origin: "https://nustrive.vercel.app", // Allow requests from this origin
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
     optionsSuccessStatus: 200,
