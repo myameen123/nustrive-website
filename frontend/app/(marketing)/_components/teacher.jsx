f16746777e3efffaac7b3de014f7bbc9ea9d63de
alimurad21 <alimurad3422@gmail.com>
2024-10-19 17:00:51 +0500

    updated teachers card

--- frontend/app/(marketing)/_components/teacher.jsx
+++ frontend/app/(marketing)/_components/teacher.jsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 const Teacher = ({teacher}) => {
   return (
-    <div className="pb-2 mx-auto bg-[#ffffff]  rounded-[12px] flex flex-col items-start border border-2-[#E6E4FF] transition-shadow "> {/*h-[490px]  md:w-[350px] w-[330px]*/}
-      <div>
-        <Image src={teacher.photo} alt="exam img" className="w-[950px]  rounded-[12px] " />
+    <div className="pb-2 mx-auto bg-[#ffffff]  rounded-[12px] flex flex-col items-start border border-2-[#E6E4FF] transition-shadow "> {/*h-[490px] */}
+      <div className=''>
+        <Image src={teacher.photo} alt="exam img" className="h-[20rem] w-auto rounded-[12px] " />
       </div>
       <div className="pt-4 pl-2  flex flex-col items-start  gap-2">
         <h2 className=" text-3xl font-bold text-[#282828] ">{teacher.name}</h2>
--- frontend/app/(marketing)/_components/teachers.jsx
+++ frontend/app/(marketing)/_components/teachers.jsx
@@ -2,12 +2,7 @@
 import React from "react";
 import Teacher from "./teacher";
 import Tutors from "../../../constants/teachers";
-// import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/free-mode";
-// import { FreeMode, Pagination } from "swiper/modules";
-// import Link from "next/link";
+import Link from "next/link";
 
 const Teachers = ({  }) => {
   return (
@@ -20,13 +15,13 @@ const Teachers = ({  }) => {
           Connect with Top Educators for Entry Level Exams Success
         </p>
       </div>
-      <div>
-        {/* <div className="sm:mr-20 sm:ml-20 lg:mr-32 lg:ml-32 flex justify-between">
+      <div className=" ">
+        <div className="mr-10 ml-10 sm:mr-20 sm:ml-20 lg:mr-32 lg:ml-32 block sm:flex justify-between max-w-[80%] sm:max-w-[90%] md:max-w-[80%] lg:max-w-[75%] ">
           <h1 className="text-2xl">Top Certified Tutors</h1>
-          <Link href={"/tutors"} className="text-[#4463fb] hover:text-red-900">
+          <Link href={"/tutors"} className="text-[#4463fb] hover:text-red-900 hidden sm:flex">
             Show All Tutors
           </Link>
-        </div> */}
+        </div>
         <div className="flex items-center justify-center flex-col md:mt-6 mt-4 ">
           <div 
             className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 max-w-[90%] sm:max-w-[90%] md:max-w-[80%] lg:max-w-[80%] "
